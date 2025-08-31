69c65327fc70b5e6b6299790f8e2b6821fd707ed
parmdeep chhabra <parmdeepchhabra@gmail.com>
2025-08-31 17:45:16 -0400

    add nuke notes, and lab 4 prompt.

---
+++ GGG- Comon scripts/Nuke Docker-Cluster.ps1
@@ -0,0 +1,3 @@
+# kubectl delete deployments --all --all-namespaces
+# fuck it for now we just go into docker windows view and see waht images are running
+# the script chatgtp gave nukes a bunch of shit that brings the k8s cluster infrastructure
