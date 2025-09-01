f500d56ee1d0c2d21da9dfcd8d78226bf939b341
parmdeep chhabra <parmdeepchhabra@gmail.com>
2025-08-31 22:41:53 -0400

    update repo addy to oci instead of whatever because of whtever someone at bitnami in the middle of somethign fuck them.

--- AAA-WorkProduct/exercise-2-helm-nginx/main.tf
+++ AAA-WorkProduct/exercise-2-helm-nginx/main.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "nginx" {
   name       = "nginx-demo"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
   chart      = "nginx"
+  version    = "21.1.23"   # Or whatever your helm search showed
   namespace  = "default"
-  version = "21.1.23"
 
   values = [
     <<-EOF
