88b6d6ff9ba1cc01953397eaf87ace0e3438458a
joethejoeyy <iam.jk.us@gmail.com>
2025-09-02 16:54:33 -0400

    Add Ingress demo steps and configuration for nginx service

--- AAA-WorkProduct/exercise-2-helm-nginx/main.tf
+++ AAA-WorkProduct/exercise-2-helm-nginx/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nginx" {
   name       = "nginx-demo"
-  repository = "oci://registry-1.docker.io/bitnamicharts"
+  repository = "oci://recleargistry-1.docker.io/bitnamicharts"
   chart      = "nginx"
   version    = "21.1.23"   # Or whatever your helm search showed
   namespace  = "default"
